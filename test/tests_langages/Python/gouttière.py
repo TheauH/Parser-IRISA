c1314abac7904aa262e9e8a13040599072658b11
Antoine Jamelot <antoine.jamelot@outlook.fr>
2021-02-16 14:59:59 +0100

    Début et fin de gouttière : pourquoi ça ne marche pas ?

--- test/tests_langages/Python/gouttière.py
+++ test/tests_langages/Python/gouttière.py
@@ -5,7 +5,25 @@ class Gouttière:
     et sa fin (indice de sa dernière ligne)
     """
 
-    def __init__(self, abscisse: int, première_ligne: int, dernière_ligne: int) -> None:
+    def __init__(self, abscisse: int, première_ligne: int, dernière_ligne: int):
         self.abscisse = abscisse
         self.début = première_ligne
         self.fin = dernière_ligne
+
+    def get_abscisse(self):
+        return self.abscisse
+
+    def set_abscisse(self, n: int):
+        self.abscisse = n
+
+    def get_début(self):
+        return self.début
+
+    def set_début(self, n: int):
+        self.début = n
+
+    def get_fin(self):
+        return self.fin
+
+    def set_fin(self, n: int):
+        self.fin = n
--- test/tests_langages/Python/page.py
+++ test/tests_langages/Python/page.py
@@ -1,14 +1,12 @@
 from typing import List
 from gouttière import Gouttière
-from trouver_la_gouttière import vacuité_colonne
-
 
 class Page(List[str]):
     """
     Représente une page de texte, composée d’une chaîne de caractères par ligne.
     """
 
-    def __init__(self, texte: str) -> None:
+    def __init__(self, texte: str):
         for ligne in texte.split("\n"):
             self.append(ligne)
 
@@ -19,11 +17,11 @@ class Page(List[str]):
         """
         l = 0
         for ligne in self:
-            if len(ligne) > largeur:
-                largeur = len(ligne)
+            if len(ligne) > l:
+                l = len(ligne)
         return l
 
-    def vacuité_colonne(self, colonne: int) -> int:
+    def vacuité_colonne(self, colonne: int):
         """Nombre de vides dans une colonne de la page"""
         vides = 0
         for ligne in self:
@@ -35,6 +33,7 @@ class Page(List[str]):
         """
         Trouve la gouttière séparant les deux colonnes.
         """
+        # 1. Recherche de l’abscisse de la gouttière
         largeur = self.largeur()
         # Largeur, à gauche et à droite, sur laquelle on ne cherchera pas la gouttière
         marge = largeur // 4
@@ -53,13 +52,33 @@ class Page(List[str]):
                 max_vide = carte_du_vide[x]
                 colonne_max_vide = x
 
-        if colonne_max_vide:
-            colonne_max_vide += (
-                marge  # On n’oublie pas le décalage dû à la marge non recherchée
-            )
-            return Gouttière(colonne_max_vide, 0, len(self) - 1)  # À corriger
-        else:
+        # Si aucune colonne n’a été plus vide que le seuil, la recherche s’arrêti ici.
+        if not colonne_max_vide:
             return None
+
+        # 2. Recherche des ordonnées de la gouttière
+        """
+        Algorithme :
+        On remonte le début, et on abaisse la fin, jusqu’à buter sur du texte.
+        """
+        début = fin = len(self) // 2
+        while début:
+            if (
+                len(self[début]) > colonne_max_vide
+                and self[début][colonne_max_vide] != " "
+            ):
+                print("bute sur le caractère", self[début][colonne_max_vide])
+                début += 1
+                break
+            début -= 1
+        while fin < len(self) - 1:
+            if len(self[fin]) > colonne_max_vide and self[fin][colonne_max_vide] != " ":
+                print("bute sur le caractère", self[fin][colonne_max_vide])
+                fin -= 1
+                break
+            fin += 1
+
+        return Gouttière(colonne_max_vide, début, fin)  # C’est prêt
 
 
 """ Exemple

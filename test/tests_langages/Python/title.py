ade54724b983c740fbe8dbf61d9c4147f96fe020
HyziOne <72913682+HyziOne@users.noreply.github.com>
2021-02-19 13:49:17 +0100

    Add files via upload

--- test/tests_langages/Python/title.py
+++ test/tests_langages/Python/title.py
@@ -9,57 +9,54 @@ import PyPDF2
 import re
 
 def extract_information(pdf_path):
+    #Récupération des métadonnées 
     with open(pdf_path, 'rb') as f:
         pdf = PdfFileReader(f)
         information = pdf.getDocumentInfo()
-
     txt = f"""Titre : {information.title}"""
-    if len(txt) <=12 :
-        
+    #si le titre est égale à None ou null alors vient forcer la recherche de celui ci
+    if len(txt) <=12 or txt.startswith("Titre : /"):
+        #parcours première page du texte
         pdfFileObj = open(pdf_path, 'rb')
         pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
         pageObj = pdfReader.getPage(0)
         txt2 = pageObj.extractText()
-        
+        #Recupération des premières lignes du texte (le titre)
         msg_splitlines = txt2.splitlines()
         headerTo = msg_splitlines[0]
         headerFrom= msg_splitlines[1]
-        
-        concatenation = headerTo +headerFrom
-
+        #Concatenation des 2 premières lignes du texte
+        concatenation = headerTo +headerFrom 
+        #Regex pour venir rajouter un espace entre chaque majuscule de la chaine de caractère
         p = re.compile(r'([a-z])([A-Z])')
         title = re.sub(p, r"\1 \2", concatenation)
-        print("Titre : "+title)
+        return "Titre : "+title
     else :
-        print(txt)
+        return txt
     return information
 
 
 if __name__ == '__main__':
     path = r"C:\Users\sosob\Desktop\S6\Projet de dev\Corpus_2021\Boudin-Torres-2006.pdf"
-    extract_information(path)
+    print(extract_information(path))
     path = r"C:\Users\sosob\Desktop\S6\Projet de dev\Corpus_2021\Das_Martins.pdf"    
-    extract_information(path)
+    print(extract_information(path))
     path = r"C:\Users\sosob\Desktop\S6\Projet de dev\Corpus_2021\Gonzalez_2018_Wisebe.pdf"
-    extract_information(path)
+    print(extract_information(path))
     path = r"C:\Users\sosob\Desktop\S6\Projet de dev\Corpus_2021\Iria_Juan-Manuel_Gerardo.pdf"    
-    extract_information(path)
-   
+    print(extract_information(path))
     path = r"C:\Users\sosob\Desktop\S6\Projet de dev\Corpus_2021\kessler94715.pdf"    
-    extract_information(path)
+    print(extract_information(path))
     path = r"C:\Users\sosob\Desktop\S6\Projet de dev\Corpus_2021\kesslerMETICS-ICDIM2019.pdf"
-    extract_information(path)
+    print(extract_information(path))
     path = r"C:\Users\sosob\Desktop\S6\Projet de dev\Corpus_2021\mikheev J02-3002.pdf"    
-    extract_information(path)
+    print(extract_information(path))
     path = r"C:\Users\sosob\Desktop\S6\Projet de dev\Corpus_2021\Mikolov.pdf"
-    extract_information(path)
+    print(extract_information(path))
     path = r"C:\Users\sosob\Desktop\S6\Projet de dev\Corpus_2021\Nasr.pdf"    
-    extract_information(path)
+    print(extract_information(path))
     path = r"C:\Users\sosob\Desktop\S6\Projet de dev\Corpus_2021\Torres.pdf"
-    extract_information(path)
+    print(extract_information(path))
     path = r"C:\Users\sosob\Desktop\S6\Projet de dev\Corpus_2021\Torres-moreno1998.pdf"    
-    extract_information(path)    
-    
-    #text_extractor(path)
-    
+    print(extract_information(path))
     

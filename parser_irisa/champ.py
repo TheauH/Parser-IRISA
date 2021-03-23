893377d667135c56f036c1fd6a51a4b7df4c9a9a
Antoine Jamelot <antoine.jamelot@outlook.fr>
2021-03-23 15:37:17 +0100

    Ajout d’une classe Champ

---
+++ parser_irisa/champ.py
@@ -0,0 +1,20 @@
+class Champ:
+    """
+    Représente un champ d’article : titre, auteur, résumé...
+    """
+
+    def __init__(
+        self, contenu, page_début: int, ligne_début: int, page_fin: int, ligne_fin: int
+    ):
+        """
+        Le contenu du champ (texte ou autre) doit se retrouver dans sa transcription
+        dans l’intervalle :
+
+        [ `transcription[page_début][ligne_début]` ; `transcription[page_fin][ligne_fin]` [
+        (début inclus, fin exclue, comme de coutume en Python)
+        """
+        self.contenu = contenu
+        self.page_début = page_début
+        self.ligne_début = ligne_début
+        self.page_fin = page_fin
+        self.ligne_fin = ligne_fin

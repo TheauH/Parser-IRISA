771a9c0c99b3f6a6a77821789eb99391296ba53c
HyziOne <72913682+HyziOne@users.noreply.github.com>
2021-03-30 14:59:35 +0200

    Add files via upload

--- parser_irisa/conclusion.py
+++ parser_irisa/conclusion.py
@@ -6,25 +6,41 @@ def find_conclusion(transcri: Transcription):
     string = "Conclusion"
     string2 = "Results"
     string3 = "CONCLUSION"
-    
+    string4 = "Acknowledgement"
+    string5 = "Acknowledgment"
+    string6 = "Reference"
+    string7 = "REFERENCE"
+
     """declaration variable contenant le résultat """
     
     """compteur de page"""
     # Page et numéro de la ligne qui nous intéressent
     ligne = 0
     page = 0
+    ligne1 = 0
+    page1 = 0
+    i=0
+    k=0
     for np in range(len(transcri) - 1, -1, -1):
-        for nl in range(len(transcri[np])):
-            if (
-                (string in transcri[np][nl])
-                or (string2 in transcri[np][nl])
-                or (string3 in transcri[np][nl])
-            ):
+        for nl in range(len(transcri[np])):    
+            if ((string in transcri[np][nl]) or (string2 in transcri[np][nl]) or (string3 in transcri[np][nl]) ):
                 page = np
                 ligne = nl
                 break
-        if page and ligne:
+   
+            if ((string4 in transcri[np][nl]) or (string5 in transcri[np][nl]) or (string6 in transcri[np][nl]) or (string7 in transcri[np][nl])):
+                page1 = np
+                ligne1 = nl
+                break  
+
+        
+        if page and ligne or page1 and ligne1: 
             break
+    
+    
+    #while et ajouter ligne par ligne
+    
+
     # n = 0
     # on se positionne à la page p et après la ligne n et on renvoie 3 lignes :
     # for ligne in transcri[-p]:
@@ -34,7 +50,8 @@ def find_conclusion(transcri: Transcription):
     #     else:
     #         j -= 1
     # buf += "..."
-    return "\n".join(transcri[page][ligne : ligne + 3]) + "…"
+    return "\n".join(transcri[page][ligne : ligne1 - ligne - 1])
+    
 
 
 """Test sur un document"""

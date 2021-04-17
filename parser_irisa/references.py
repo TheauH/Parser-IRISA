d5833966e38b8cc556cca75b0a3e06d5d5cca68f
HyziOne <72913682+HyziOne@users.noreply.github.com>
2021-04-17 13:45:07 +0200

    Add files via upload

--- parser_irisa/references.py
+++ parser_irisa/references.py
@@ -12,28 +12,64 @@ def find_references(transcri: Transcription):
     string2 = "REFERENCES"
     string3 = "Références"
     string4 = "R EFERENCES"
+    stringend = "ù"
     """declaration variable contenant le résultat """
     """compteur de page"""
     # Page et numéro de la ligne qui nous intéressent
-    page = None
     ligne = 0
-    for np, p in reversed(list(enumerate(transcri))):
-        for nl, l in enumerate(p):
-            if string in l or string2 in l or string3 in l:
-                page = p
+    page = 0
+    ligne1 = 0
+    page1 = 0
+    i = 0
+    k = 0
+    for np in range(len(transcri) - 1, -1, -1):
+        for nl in range(len(transcri[np]) - 1, -1, -1):
+            if (
+                (string in transcri[np][nl])
+                or (string2 in transcri[np][nl])
+                or (string3 in transcri[np][nl])
+                or (string4 in transcri[np][nl])
+                ):
+                page = np
                 ligne = nl
-                # Champ trouvé
-                return Champ(
-                    nom="biblio",
-                    contenu="\n".join(page[0:][ligne + 1 : ligne + 100]),
-                    page_début=np,
-                    ligne_début=ligne + 1,
-                    page_fin=np,
-                    ligne_fin=ligne + 100,
-                )
+            if stringend in transcri[np][nl]:
+                page1 = np
+                ligne1 = nl
 
-    # Champ non trouvé
-    return None
+        # sort de la boucle après que page et page1 soit trouvé
+        if page and page1:
+            break
+
+    # si le début n'est pas trouvé, rendre vide
+    if page == 0:
+        return None
+
+    if page > page1:
+        result = "\n".join(transcri[page][ligne : len(transcri[page]) - 1])
+        ligne = 0
+        page += 1
+    else:
+        return Champ(
+            nom="Reference",
+            contenu="\n".join(transcri[page][ligne : ligne1 - 1]),
+            page_début=page,
+            ligne_début=ligne,
+            page_fin=page,
+            ligne_fin=ligne1 - 1,
+        )
+
+    while page < page1:
+        result += "\n".join(transcri[page][0 : len(transcri[page]) - 1])
+        page += 1
+    return Champ(
+        nom="Reference : ",
+        contenu=result + "\n".join(transcri[page][ligne : ligne1 - 1]),
+        page_début=page,
+        ligne_début=ligne,
+        page_fin=page,
+        ligne_fin=ligne1 - 1,
+    )
+
 
 
 """Test sur un document"""

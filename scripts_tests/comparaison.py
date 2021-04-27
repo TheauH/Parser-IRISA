dc14692f299e95f17784d81dd9b9644f1f3a97b5
Antoine Jamelot <antoine.jamelot@outlook.fr>
2021-04-27 15:54:24 +0200

    Script de comparaison non terminé mais fonctionnel

--- scripts_tests/comparaison.py
+++ scripts_tests/comparaison.py
@@ -1,11 +1,13 @@
 from typing import Union
-from .article import Article
-from os import PathLike
+from os import PathLike, scandir
+from pathlib import Path
 import xml.etree.ElementTree as ET
-from .strop import sans_blancs
+from config import strop, article
 
 
-def précision(art: Article, ref: Union[str, bytes, PathLike], souple=False) -> float:
+def précision(
+    art: article.Article, ref: Union[str, bytes, PathLike], souple=False
+) -> float:
     """
     Donne la précision de l’article `art`, calculée comme suit :
     Précision=Sections correctes trouvées par le système / Sections véritables.
@@ -22,15 +24,29 @@ def précision(art: Article, ref: Union[str, bytes, PathLike], souple=False) -> 
         "results": art.results,
         "discussion": art.discussion,
         "conclusion": art.conclusion,
+        "biblio": art.references
     }
     sections_correctes = sections_attendues = 0
-    for élément in refXML.iter():
+    for élément in refXML:
         # TODO Mieux prendre en compte le cas de la section « auteurs »
         sections_attendues += 1
-        contenu = sans_blancs(
+        contenu = strop.sans_blancs(
             "".join(élément.itertext()) if élément.tag == "auteurs" else élément.text
         )
-        if contenu == sans_blancs(champs_art[élément.tag].contenu):
+        if contenu == strop.sans_blancs(str(champs_art[élément.tag].contenu)):
             sections_correctes += 1
 
     return sections_correctes / sections_attendues
+
+
+chemin_PDF = Path("test/Corpus TEST")
+dossier_PDF = scandir(chemin_PDF)
+dossier_résultats = Path("test/Corpus TEST/Parsage manuel")
+
+for entrée_PDF in dossier_PDF:
+    if entrée_PDF.name.endswith(".pdf") and entrée_PDF.is_file:
+        art = article.Article(chemin_PDF / entrée_PDF.name)
+        print(
+            entrée_PDF.name,
+            précision(art, dossier_résultats / (entrée_PDF.name[:-3] + "xml")),
+        )
